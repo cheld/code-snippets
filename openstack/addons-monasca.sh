ad7acce04638dfa0d8f9b718e30533e9c8a5627c
Christoph Held <c-held@web.de>
2016-09-28 13:55:47 +0200

    fix monasca link

--- openstack/addons-monasca.sh
+++ openstack/addons-monasca.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Runs Heapster in standalone mode
-docker run --net=host -d gcr.io/google_containers/heapster:v1.2.0 -port 8082 \
+docker run --net=host -d gcr.io/google_containers/heapster:v1.1.0 -port 8082 \
     --source="kubernetes:http://127.0.0.1:8080?inClusterConfig=false&auth=" \
-    --sink="monasca:?user-id=ff69eeb3fd3b412a917be02ecc833963&password=password&keystone-url=http://10.140.18.87:5000/v3"
+    --sink="monasca:?user-id=ff69eeb3fd3b412a917be02ecc833963&password=password&keystone-url=http://127.0.0.1:5000/v3"
 
 sleep 5
 
