05f9e155001bcb58d03d5cf3b320d8211ba9985e
Christoph Held <christoph.held@est.fujitsu.com>
2016-12-23 08:30:49 +0100

    vpn

--- openstack/addons.sh
+++ openstack/addons.sh
@@ -12,4 +12,4 @@ docker run --net=host -d fest/heapster:v1.2.0 \
 docker run --net=host -d gcr.io/google_containers/kubernetes-dashboard-amd64:v1.4.0 --apiserver-host=http://127.0.0.1:8080 \
     --port 9090 --heapster-host=http://127.0.0.1:8082 
 
-docker run --net=host -it -d -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /var/log/containers:/var/log/containers:ro taimir93/logstash-monasca:v1.1 /run.sh http://127.0.0.1:5607/v3.0 http://127.0.0.1:5000/v3 mini-mon monasca-agent password Default
+#docker run --net=host -it -d -v /var/lib/docker/containers:/var/lib/docker/containers:ro -v /var/log/containers:/var/log/containers:ro taimir93/logstash-monasca:v1.1 /run.sh #http://127.0.0.1:5607/v3.0 http://127.0.0.1:5000/v3 mini-mon monasca-agent password Default
